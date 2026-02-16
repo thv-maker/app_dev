8b58c2ac05ef6802931cf3afa9d3596b41debc92
Kikay-dotcom <jimwellalomotosdeguzman@gmail..com>
2026-02-16 18:47:37 +0800

    Initial commit

---
+++ src/screens/auth/login.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Alert, Text, View } from 'react-native';
+
+import CustomButton from '../../components/CustomButton';
+import CustomTextInput from '../../components/CustomTextInput';
+import { useNavigation } from '@react-navigation/native';
+
+const Login = () => {
+  // GETTER //SETTER
+  const [emailAdd, setEmailAdd] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigation = useNavigation()
+
+  //   useEffect(() => {}, [emailAdd, password]);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'black' }}>{emailAdd}</Text>
+      <Text style={{ color: 'black' }}>{password}</Text>
+
+      <CustomTextInput
+        label={'Email Address'}
+        placeholder={'Email Address'}
+        value={val => setEmailAdd(val)}
+        containerStyle={{
+          width: '100%',
+        }}
+        labelStyle={{
+          fontSize: 20,
+          fontWeight: '500',
+        }}
+        textStyle={{
+          fontSize: 20,
+        }}
+      />
+
+      <CustomTextInput
+        label={'Password'}
+        placeholder={'Password'}
+        value={val => setPassword(val)}
+        containerStyle={{
+          width: '100%',
+        }}
+        labelStyle={{
+          fontSize: 20,
+          fontWeight: '500',
+        }}
+        textStyle={{
+          fontSize: 20,
+        }}
+      />
+
+      <CustomButton label={'LOGIN'} onPress={() => {
+        if (emailAdd) Alert.alert("Incorrect Credentials", "Please try again!");
+
+
+        
+
+
+
+      }} />
+    </View>
+  );
+};
+
+export default Login;
---
+++ src/utils/image.js
@@ -0,0 +1,3 @@
+export default {
+  LOGO: 'https://tse4.mm.bing.net/th/id/OIP._rMiN4neclDygd20ZbR7ggHaEK?w=3840&h=2160&rs=1&pid=ImgDetMain&o=7&rm=3',
+};
